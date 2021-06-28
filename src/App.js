ceaaa535b65c4fe58b6fbd98fc4025fe1beef59a
zackinfosbd <zackinfosbd@gmail.com>
2021-06-28 19:35:32 +0100

    last final commit message

--- src/App.js
+++ src/App.js
@@ -6,19 +6,46 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+
   const handleSubmit = (e) => {
-    e.preventDefaul();
-    console.log("hello");
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+    }
   };
+
   return (
     <>
       <section className="container">
         <h3>color generator</h3>
-        <form onSubmit={handleSubmit}></form>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            // placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
